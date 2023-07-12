fce4272c53ad4dd8c82ff4ad8a7be7a468c98248
sacross93 <wlsdud1517@naver.com>
2023-07-13 00:02:43 +0900

    dummy var error
    finish input list
    - basic patients information
    - ward BP
    - anesthesia method
    - anesthesia inducer
    - anesthesia maintenance agent
    
    input list
    - anesthetic

--- Preprocessing/07_Diagnosis_dummy.py
+++ Preprocessing/07_Diagnosis_dummy.py
@@ -8,3 +8,17 @@ diagnosis = pd.read_csv(preprocessing_path+'02_Diagnosis_split.csv')
 
 test = pd.get_dummies(diagnosis, columns=['IDC'])
 test.to_csv(preprocessing_path+'07_Diagnosis_dummies.csv', index=False, encoding='utf-8-sig')
+
+len(test['마취기록작성번호'].unique())
+
+test[test['마취기록작성번호'] == 20190700021805]
+
+# aa = test.groupby('마취기록작성번호')
+# bb = aa.max().reset_index(drop=True)
+# bb
+
+for pat in test['마취기록작성번호'].unique():
+    temp_data = test[test['마취기록작성번호'] == pat]
+    for key in temp_data.keys():
+        if key == '마취기록작성번호':
+            continue
