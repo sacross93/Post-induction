dbfe85e913c74a1e4d9ea9211a1b8835daf331c8
sacross93 <68053552+sacross93@users.noreply.github.com>
2023-07-13 09:50:14 +0900

    finish fix dummy var error
    finish input list
    - basic patients information
    - ward BP
    - anesthesia method
    - anesthesia inducer
    - anesthesia maintenance agent
    
    input list
    - anesthetic
    - emergency
    - pre-induction bp

--- Preprocessing/07_Diagnosis_dummy.py
+++ Preprocessing/07_Diagnosis_dummy.py
@@ -1,24 +1,31 @@
 import pandas as pd
 import numpy as np
 
-preprocessing_path = 'C:/Users/user/Dropbox/김진영_SignalHouse/0_마취기록지/Post-induction/input_preprocessing/'
-original_path = 'C:/Users/user/Dropbox/김진영_SignalHouse/0_마취기록지/original_data/'
+preprocessing_path = '/srv/project_data/EMR/jy/Post-induction/Input_preprocessing/'
+original_path = '/srv/project_data/EMR/original_data/'
 
 diagnosis = pd.read_csv(preprocessing_path+'02_Diagnosis_split.csv')
 
 test = pd.get_dummies(diagnosis, columns=['IDC'])
-test.to_csv(preprocessing_path+'07_Diagnosis_dummies.csv', index=False, encoding='utf-8-sig')
+# test.to_csv(preprocessing_path+'07_Diagnosis_dummies.csv', index=False, encoding='utf-8-sig')
 
 len(test['마취기록작성번호'].unique())
 
 test[test['마취기록작성번호'] == 20190700021805]
 
-# aa = test.groupby('마취기록작성번호')
-# bb = aa.max().reset_index(drop=True)
-# bb
+aa = test.groupby('마취기록작성번호')
+bb = aa.max().reset_index()
 
-for pat in test['마취기록작성번호'].unique():
-    temp_data = test[test['마취기록작성번호'] == pat]
-    for key in temp_data.keys():
+bb.to_csv(preprocessing_path+"07_Diagnosis_dummies.csv", index=False, encoding='utf-8-sig')
+
+
+#### dummies test
+for data in bb['마취기록작성번호'].unique():
+    for key in bb.keys():
         if key == '마취기록작성번호':
+            temp_key = bb[bb[key] == data]
             continue
+        tdd = temp_key[key].to_numpy()[0]
+        if tdd != 0:
+            print(data, temp_key[key])
+
